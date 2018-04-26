6dec4c174b8ccaa8bf0206bce1367a22cfe2cab0
huangqi <385424446@qq.com>
2018-04-26 18:36:24 +0800

    URI

--- studyProj/src/main/java/Network/URL/MainClass.java
+++ studyProj/src/main/java/Network/URL/MainClass.java
@@ -3,14 +3,12 @@ package Network.URL;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.net.*;
 import java.util.Arrays;
 import java.util.Enumeration;
 
 public class MainClass {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         MainClass mainClass = new MainClass();
          /*URL url = new URL("http://www.baidu.com/id/1.html");
         System.out.println(url.getProtocol());
@@ -19,8 +17,46 @@ public class MainClass {
 
         File file = new File("C:/HQ.ini");
         System.out.println(file.toURL().toString());*/
-        mainClass.testGetContent();
+        //mainClass.testGetContent();
+
+        /*URI uri = new URI("http://www.baidu.com");
+
+        if (uri.isOpaque()){ //Opaque：不透明的 ，有层次时返回false，没层次时返回true
+            System.out.println("isOpaque");
+        }else{
+            System.out.println("notOpaque");
+        }
+        if (uri.isAbsolute()){ //绝对
+            System.out.println("isAbsolute");
+        }else{
+            System.out.println("notAbsolute");
+        }*/
+        mainClass.testresolve();
     }
+
+    public void testresolve() throws URISyntaxException {
+        URI uri = new URI("http://www.baidu.com");
+        URI uri1 = new URI("/aa/index");
+        System.out.println(uri.resolve(uri1)); //1+0
+        System.out.println(uri1.resolve(uri));
+
+        URI uri2 = new URI("http://www.baidu.com/aa/index");
+        URI uri3 = new URI("http://www.baidu.com");
+        System.out.println(uri2.relativize(uri3));
+        System.out.println(uri3.relativize(uri2));// 2-3
+        try {
+            InputStream inputStream = uri3.toURL().openStream();
+            int c;
+            byte[] arr = new byte[64];
+            while ((c=inputStream.read(arr))!=-1){
+                System.out.println(new String(arr));
+                Arrays.fill(arr, (byte) 0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void testGetContent() throws IOException {
         URL url = new URL("http://www.baidu.com");
         Object o = url.getContent();
