8927a6175602316f9db5874361409dbc070f8e1c
huangqi <385424446@qq.com>
2018-05-09 10:10:22 +0800

    tomcat学习

---
+++ tomcat/src/main/java/ex02/pyrmont/Constants.java
@@ -0,0 +1,9 @@
+package ex02.pyrmont;
+
+import java.io.File;
+
+public class Constants {
+  public static final String WEB_ROOT =
+    //System.getProperty("user.dir") + File.separator  + "webroot";
+          System.getProperty("user.dir") + File.separator  + "target"+ File.separator  + "classes";
+}
